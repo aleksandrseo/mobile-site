5cdb09d436cfae8074e17f90dd2bcd52ecaa4ecd
user <user@pc-computer.(none)>
2012-01-04 13:07:55 +0400

    04.01.12

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,16 @@
 <?
-if (!m::request('menu')) {
+$menu = m::request('menu');
+$core = m::request('core');
+
+if ($menu) {
+    require('models/' . $menu . '.php');
+    require('views/' . $menu . '.php');
+} else if ($core) {
+    require('models/' . $core . '.php');
+} else  {
+    require("models/index.php");
     require("views/index.php");
-} else {
-    require('views/' . m::request('menu') . '.php');
 }
+
+
 ?>
