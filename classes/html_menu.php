1e7c9ec5dd3f8009729bb61eaa833495ba3a89b7
user <user@pc-computer.(none)>
2012-01-08 09:18:08 +0400

    08.01.12

--- classes/html_menu.php
+++ classes/html_menu.php
@@ -1,9 +1,12 @@
 <?
 class html_menu {
     function __construct() {
-     $this->enter = $this->hyperLink('Вход','enter');
+     $this->enter = $this->link('Вход','enter');
+     $this->registration = $this->link('Регистрация','registration');
+     $this->aboutSite = $this->link('О сайте','about_site');
+     $this->aboutUs = $this->link('О нас','about_us');
     }
-    public function hyperLink($text,$link,$type = 'view'){
+    public function link($text,$link,$type = 'view'){
      return '<a href="index.php?' . $type . '=' . $link . '">' . $text . '</a><br/>';
     }
 }
