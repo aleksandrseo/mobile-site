7ca647963a79e74813eeb703a3916040c0e335e2
user <user@pc-computer.(none)>
2012-01-08 09:38:28 +0400

    08.01.12

--- classes/html_menu.php
+++ classes/html_menu.php
@@ -1,12 +1,12 @@
 <?
 class html_menu {
-    function __construct() {
+    public function __construct() {
      $this->enter = $this->link('Вход','enter');
      $this->registration = $this->link('Регистрация','registration');
      $this->aboutSite = $this->link('О сайте','about_site');
      $this->aboutUs = $this->link('О нас','about_us');
     }
-    public function link($text,$link,$type = 'view'){
+    private function link($text,$link,$type = 'view'){
      return '<a href="index.php?' . $type . '=' . $link . '">' . $text . '</a><br/>';
     }
 }
